011179b9be269fe5ee7d536b3f015b682ce280c6
Jatin Modi <44528244+jatinmodi001@users.noreply.github.com>
2019-07-29 16:17:36 +0530

    Create AreaOfTriangle.cpp

---
+++ AreaOfTriangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int base,height;
+	cout<<"Enter base and height of triangle\n";
+	cin>>base>>height;
+	float area;
+	area=(float)(base*height)/2;
+	cout<<area;
+	return 0;
+}
